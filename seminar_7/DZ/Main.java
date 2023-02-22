35b8b95238251c72a9026931ff552020b18fe88e
Vasiliy Maletin <maletinvasilii@mail.ru>
2023-02-22 23:54:04 +0800

    completed dz seminar_7

--- seminar_7/DZ/Main.java
+++ seminar_7/DZ/Main.java
@@ -2,6 +2,9 @@ package DZ;
 
 public class Main {
     public static void main(String[] args) {
-        new Start().start();
+        DatabaseManager databaseManager = new DatabaseManager();
+        Operation operation = new SQLOperation(databaseManager);
+        Notes notes = new Notes(operation);
+        Console console = new Console(notes);
    }
 }
